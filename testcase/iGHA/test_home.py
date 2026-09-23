67f873b22aadd9066744cc5a1085a12a62dfc721
TzuEn-Lin <enlin@google.com>
2026-09-23 16:13:19 +0800

    updated v30 version

--- testcase/iGHA/test_home.py
+++ testcase/iGHA/test_home.py
@@ -26,4 +26,4 @@ class TestGHAHome(BaseTestCase):
         gha_session.GHASession.handle_verify_camera_live_stream_and_remove(self)
         gha_session.GHASession.refresh_gha_devices(self)
         assert gha_session.GHASession.stop_gha(self)
-        time.sleep(15)
+        time.sleep(10)
