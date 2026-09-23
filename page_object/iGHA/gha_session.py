d8d97a0f7b639f08f42a9a23180d6c62f5da0209
TzuEn-Lin <enlin@google.com>
2026-09-23 15:55:54 +0800

    updated v29 version

--- page_object/iGHA/gha_session.py
+++ page_object/iGHA/gha_session.py
@@ -297,6 +297,7 @@ class GHASession:
             GHACameraLivePage.verify_camera_live_stream(self)
         finally:
             self.handle_remove_device()
+            GHACommissioningPageObject.power_cycle_smart_plug(self)
 
     def handle_remove_device(self) -> None:
         """Navigate to Settings and completely remove/unpair the camera device."""
